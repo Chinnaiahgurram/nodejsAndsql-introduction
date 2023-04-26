d037cc1b5e998e66014d79b045e3c6e30f19b95c
chinnaiahgurram <chinnaiah.gurram@gmail.com>
2023-04-26 15:22:12 +0000

    Hello Abhi

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const dbPath = path.join(__dirname, "goodreads.db");
+const app = express();
+
+let db = null;
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("This server at http://localhost/3000");
+    });
+  } catch (e) {
+    console.log(`db error ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `
+    SELECT * FROM book ORDER BY book_id`;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
